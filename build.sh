49cfc03b309dde2ff8e3d9e8ec98f5268a7f559e
Frank Wunderlich <frank-w@public-files.de>
2020-12-21 11:58:44 +0100

    build.sh: create defconfig instead of modifying .config

--- build.sh
+++ build.sh
@@ -2,13 +2,18 @@
 set -e
 logfile=output.log
 
-exec > >(cat >> $logfile)
-exec 2> >(tee -a $logfile >&2)
+#exec > >(cat >> $logfile)
+#exec 2> >(tee -a $logfile >&2)
+
+grep -v 'BR2_aarch64\|BR2_arm\|BR2_cortex' configs/BPI-R64_defconfig > configs/BPI-R2_defconfig
+sed -i '1 i\BR2_arm=y\nBR2_cortex_a7=y' configs/BPI-R2_defconfig
 
 make BPI-R64_defconfig
 make -s
 mv output/images/rootfs.cpio.gz rootfs_bpi-r64.cpio.gz
+
 make clean
-echo -e "BR2_arm=y\nBR2_cortex_a7=y" >> .config
+make BPI-R2_defconfig
+
 make -s
 mv output/images/rootfs.cpio.gz rootfs_bpi-r2.cpio.gz
