ff4096a4428d92e99f1ca2bed69edd7a9af70766
Frank Wunderlich <frank-w@public-files.de>
2020-12-21 11:33:20 +0100

    build.sh: rename build image + add r2-code

--- build.sh
+++ build.sh
@@ -6,4 +6,9 @@ exec > >(cat >> $logfile)
 exec 2> >(tee -a $logfile >&2)
 
 make BPI-R64_defconfig
-make
+make -s
+mv output/images/rootfs.cpio.gz rootfs_bpi-r64.cpio.gz
+make clean
+echo -e "BR2_arm=y\nBR2_cortex_a7=y" >> .config
+make -s
+mv output/images/rootfs.cpio.gz rootfs_bpi-r2.cpio.gz
