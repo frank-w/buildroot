d94a1067b2ed7d96c7c7b3a226f51a54e91d833c
Frank Wunderlich <frank-w@public-files.de>
2021-01-16 17:23:55 +0100

    build.sh: rework build.sh and add r2 config
    now it is more like my uboot/kernel/atf repo
    
    change board in build.conf and run
    
    ./build.sh importconfig
    ./build.sh build

--- build.sh
+++ build.sh
@@ -2,18 +2,49 @@
 set -e
 logfile=output.log
 
+board=bpi-r2
+
 #exec > >(cat >> $logfile)
 #exec 2> >(tee -a $logfile >&2)
 
-grep -v 'BR2_aarch64\|BR2_arm\|BR2_cortex' configs/BPI-R64_defconfig > configs/BPI-R2_defconfig
-sed -i '1 i\BR2_arm=y\nBR2_cortex_a7=y' configs/BPI-R2_defconfig
+if [[ -e "build.conf" ]];
+then
+	. build.conf
+fi
 
-make BPI-R64_defconfig
-make -s
-mv output/images/rootfs.cpio.gz rootfs_bpi-r64.cpio.gz
+case $1 in
+	"clean")
+		make clean
+	;;
+	"importconfig")
+		echo "import config for $board..."
+		if [[ "$board" == "bpi-r64" ]];then
+			make BPI-R64_defconfig
+		else
+			make BPI-R2_defconfig
+		fi
+	;;
+	""|"build")
+		echo "building for $board..."
+		make -s
+		mv output/images/rootfs.cpio.gz rootfs_${board}.cpio.gz
+	;;
+	"copy64config")
+		grep -v 'BR2_aarch64\|BR2_arm\|BR2_cortex' configs/BPI-R64_defconfig > configs/BPI-R2_defconfig
+		sed -i '1 i\BR2_arm=y\nBR2_cortex_a7=y' configs/BPI-R2_defconfig
+	;;
+	"version")
+		echo "Version: "$( git describe )
+	;;
+	"copy")
+		#cp output/images/rootfs.cpio.gz /media/data_nvme/git/kernel/BPI-R2-4.14/utils/buildroot/rootfs_$board.cpio.gz
+		if [[ -n "$targetdir" ]];
+		then
+			cp rootfs_${board}.cpio.gz ${targetdir}/
+		fi
+	;;
+	*)
+		echo "unsupported option: $1"
+	;;
+esac
 
-make clean
-make BPI-R2_defconfig
-
-make -s
-mv output/images/rootfs.cpio.gz rootfs_bpi-r2.cpio.gz
